633ff2b6c0a3e6375c7d7446048b7fa49f0a4a76
elev <elev@192.168.1.133>
2017-05-05 13:00:36 +0200

    Cart

--- WebShop/WebContent/scripts/routing.js
+++ WebShop/WebContent/scripts/routing.js
@@ -13,12 +13,13 @@
 		.when("/products", {templateUrl : "products.htm", controller: "productsController"})
 		.when("/contact", {templateUrl : "contact.htm", controller: "contactController"})
 		.when("/about", {templateUrl : "about.htm", controller: "aboutController"})
+		.when("/shoppingCart", {templateUrl : "shoppingCart.htm", controller: "shoppingCartController"})
 		.otherwise({redirectTo:"/"})
 	});
 	
 	routingApplication.controller("homeController", function($scope){
 		$scope.title = "Home";
-		$scope.description = "This is the Home page for Mesh Webshop";
+		$scope.description = "This is the Home page for MESH Webshop";
 	});
 	routingApplication.controller("productsController", function($scope){
 		$scope.title = "Products";
@@ -31,6 +32,10 @@
 	routingApplication.controller("aboutController", function($scope){
 		$scope.title = "About";
 		$scope.description = "This is the About page";
+	});
+	routingApplication.controller("shoppingCartController", function($scope){
+		$scope.title = "ShoppingCart";
+		$scope.description = "This is the ShoppingCart page";
 	});
 	
 }());
