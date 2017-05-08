74edbd76b017df87948961a2a4750dabd09875d6
Herman Dackenberg <herman.dackenberg@gmail.com>
2017-05-08 15:03:37 +0200

    .

--- WebShop/src/group1/webshop/api/database/DatabaseHandler.java
+++ WebShop/src/group1/webshop/api/database/DatabaseHandler.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javaBeans.Product;
+import group1.webshop.api.beans.*;
 
 public class DatabaseHandler {
 
@@ -23,28 +23,43 @@ public class DatabaseHandler {
 		
 	private ResultSet executeQuery(String query) {
 		
+		ResultSet rs;
+		
 		try (	Connection cn = getConnection();
 				PreparedStatement pstmt = cn.prepareStatement(query);
-				ResultSet rs = pstmt.executeQuery(query)) {
-			
+				this.rs = pstmt.executeQuery()) {
 		}
 		return rs;
 	}
-
+	
 	public Product getProductByName (String name, int price, String description) throws ClassNotFoundException, SQLException {
 		Product product = new Product();
-		
-		try (
-			Connection cn = getConnection();
-			PreparedStatement pstmt = cn.prepareStatement("SELECT id, name, description FROM product WHERE name = " + name);
-			ResultSet rs = pstmt.executeQuery();
+		ResultSet rs = executeQuery("SELECT * FROM product");
 			) {
 					rs.next();
-					product.setProductName(rs.getString("name"));
-					product.setProductPrice(rs.getInt("price"));
-					product.setProductDescription(rs.getString("description"));
+					product.setId(rs.getInt("id"));
+					product.setName(rs.getString("name"));
+					product.setDescription(rs.getString("description"));
+					product.setPrice(rs.getDouble("price"));
+					product.setManufacturer(rs.getString("manufacturer"));
 			}
 		return product;
 	}
+	
+//	public Product getProductByName (String name, int price, String description) throws ClassNotFoundException, SQLException {
+//		Product product = new Product();
+//		
+//		try (
+//			Connection cn = getConnection();
+//			PreparedStatement pstmt = cn.prepareStatement("SELECT id, name, description FROM product WHERE name = " + name);
+//			ResultSet rs = pstmt.executeQuery();
+//			) {
+//					rs.next();
+//					product.setProductName(rs.getString("name"));
+//					product.setProductPrice(rs.getInt("price"));
+//					product.setProductDescription(rs.getString("description"));
+//			}
+//		return product;
+//	}
 
 }
