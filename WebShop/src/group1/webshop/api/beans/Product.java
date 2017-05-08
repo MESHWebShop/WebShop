32d5a8e687a9242b66000f733b5bf39828b2c6b9
Ragnarsmoke <emil.bertilsson@outlook.com>
2017-05-08 15:02:21 +0200

    Added product bean

--- WebShop/src/group1/webshop/api/beans/Product.java
+++ WebShop/src/group1/webshop/api/beans/Product.java
@@ -5,7 +5,112 @@ package group1.webshop.api.beans;
  */
 public class Product extends StoredItem {
 
-    public Product() {
+    /**
+     * Product name
+     */
+    private String name;
+
+    /**
+     * Product description
+     */
+    private String description;
+
+    /**
+     * Product manafacturer
+     */
+    private String manafacturer;
+
+    /**
+     * Product categories
+     */
+    private String[] categories = new String[0];
+
+    /**
+     * Product images
+     */
+    private ProductImage[] images = new ProductImage[0];
+
+    /**
+     * Product price
+     */
+    private int price;
+
+    /**
+     * Retrieves the product name
+     *
+     * @return Product name
+     */
+    public String getName() {
+	return name;
+    }
+    
+    /**
+     * Retrieves the product description
+     *
+     * @return Product description
+     */
+    public String getDescription() {
+	return description;
+    }
+
+    /**
+     * Retrieves the product manafacturer
+     *
+     * @return Product manafacturer
+     */
+    public String getManafacturer() {
+	return manafacturer;
+    }
+
+    /**
+     * Retrieves the product categories
+     *
+     * @return Product categories
+     */
+    public String[] getCategories() {
+	return categories;
+    }
+
+    /**
+     * Retrieves the product images
+     *
+     * @return Product images
+     */
+    public ProductImage[] getImages() {
+	return images;
+    }
+
+    /**
+     * Retrieves the product price
+     *
+     * @return Product price
+     */
+    public int getPrice() {
+	return price;
+    }
+
+    /**
+     * Sets the product categories
+     *
+     * @param categories New product categories
+     */
+    public void setCategories(String[] categories) {
+	this.categories = categories;
+    }
+
+    /**
+     * Product constructor
+     *
+     * @param name Product name
+     * @param description Product description
+     * @param manafacturer Product manafacturer
+     * @param price Product price
+     */
+    public Product(String name, String description, String manafacturer, int price) {
+	this.name = name;
+	this.description = description;
+	this.manafacturer = manafacturer;
+	this.price = price;
     }
 
 }
