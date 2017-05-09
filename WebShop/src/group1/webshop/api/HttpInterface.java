8253a65c81f3667980311a686f4f72896d8dfe24
Emil <emil@EmilLinux>
2017-05-09 13:48:27 +0200

    Added HTTP/JSON interface classes

---
+++ WebShop/src/group1/webshop/api/HttpInterface.java
@@ -0,0 +1,23 @@
+package group1.webshop.api;
+
+import java.util.Map;
+
+import group1.webshop.api.JsonMapValidator;
+
+/**
+ * HTTP Interface class
+ * @author Emil Bertilsson
+ */
+public class HttpInterface {
+
+    /**
+     * Validates a JSON data map using a map validator
+     * @param validator Map validator
+     * @param jsonMap JSON data map
+     * @return Errors in JSON format
+     */
+    public String validateJsonMap(JsonMapValidator validator, Map<String, Object> jsonMap) {
+	return validator.validate(jsonMap);
+    }
+
+}
---
+++ WebShop/src/group1/webshop/api/JsonInterface.java
@@ -0,0 +1,41 @@
+package group1.webshop.api;
+
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+
+/**
+ * JSON Interface class
+ * @author Emil Bertilsson
+ */
+public class JsonInterface {
+
+    /**
+     * GSON Serializer object
+     */
+    private static final Gson GSON_OBJ = new GsonBuilder()
+	.serializeNulls()
+	.create();
+
+    /**
+     * Converts a java Object to JSON text
+     * @param obj Object to convert
+     * @return JSON text
+     */
+    public static String toJson(Object obj) {
+	return GSON_OBJ.toJson(obj);
+    }
+
+    /**
+     * Parses a JSON text to a data map
+     * @param json JSON text
+     * @return Data map
+     */
+    public static Map<String, Object> fromJson(String json)
+	throws JsonSyntaxException {
+	return (Map<String, Object>) GSON_OBJ.fromJson(json, Map.class);
+    }
+
+}
