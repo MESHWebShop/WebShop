3d45985e91bdfdb707d64fd9b05e461cd1a10e55
Sven Andersson <elev@192.168.1.112>
2017-05-10 09:53:14 +0200

    Sven har gjort Stored

--- WebShop/WebContent/scripts/routing.js
+++ WebShop/WebContent/scripts/routing.js
@@ -4,41 +4,54 @@
 
 (function() {
 
-	var routingApplication = angular.module("routingApplication", [ "ngRoute" ]);
+	var routingApplication = angular
+			.module("routingApplication", [ "ngRoute" ]);
 
 	routingApplication.config(function($routeProvider) {
-		
-		$routeProvider
-		.when("/", {templateUrl : "main.htm", controller: "homeController"})
-		.when("/products", {templateUrl : "products.htm", controller: "productsController"})
-		.when("/contact", {templateUrl : "contact.htm", controller: "contactController"})
-		.when("/about", {templateUrl : "about.htm", controller: "aboutController"})
-		.when("/shoppingCart", {templateUrl : "shoppingCart.htm", controller: "shoppingCartController"})
-		.otherwise({redirectTo:"/"})
+
+		$routeProvider.when("/", {
+			templateUrl : "main.htm",
+			controller : "homeController"
+		}).when("/products", {
+			templateUrl : "products.htm",
+			controller : "productsController"
+		}).when("/contact", {
+			templateUrl : "contact.htm",
+			controller : "contactController"
+		}).when("/about", {
+			templateUrl : "about.htm",
+			controller : "aboutController"
+		}).when("/shoppingCart", {
+			templateUrl : "shoppingCart.htm",
+			controller : "shoppingCartController"
+		}).otherwise({
+			redirectTo : "/"
+		})
 	});
-	
-	routingApplication.controller("homeController", function($scope){
+
+	routingApplication.controller("homeController", function($scope) {
 		$scope.title = "Home";
 		$scope.description = "This is the Home page for MESH Webshop";
 	});
-	routingApplication.controller("productsController", function($scope){
+
+	routingApplication.controller("productsController", function($scope) {
 		$scope.title = "Products";
 		$scope.description = "This is the Products page";
 	});
-	routingApplication.controller("contactController", function($scope){
+
+	routingApplication.controller("contactController", function($scope) {
 		$scope.title = "Contact";
 		$scope.description = "This is the Contact page";
 	});
-	routingApplication.controller("aboutController", function($scope){
+
+	routingApplication.controller("aboutController", function($scope) {
 		$scope.title = "About";
 		$scope.description = "This is the About page";
 	});
-	routingApplication.controller("shoppingCartController", function($scope){
+
+	routingApplication.controller("shoppingCartController", function($scope) {
 		$scope.title = "ShoppingCart";
 		$scope.description = "This is the ShoppingCart page";
 	});
-	
+
 }());
-
-
-
