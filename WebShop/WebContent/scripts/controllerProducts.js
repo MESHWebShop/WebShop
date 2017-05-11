c9d2126e27f0f6525e23e7764a57e2207201c815
elev <elev@192.168.1.133>
2017-05-11 13:38:11 +0200

    Edit

--- WebShop/WebContent/scripts/controllerProducts.js
+++ WebShop/WebContent/scripts/controllerProducts.js
@@ -4,8 +4,21 @@
 (function() {
 	var controllerApplication = angular.module("routingApplication");
 
-	controllerApplication.controller("productsController", function($scope) {
+	controllerApplication.controller("productsController", function($scope, $http) {
 		$scope.title = "Products";
 		$scope.description = "This is the Products page";
+		
+		var products = [];
+		$scope.products = products;
+		
+		var onProductsComplete = function(response) {
+			$scope.products = response.data;
+		}
+		
+		var onError = function(reason) {
+			$scope.name = "Could not fetch data! " + reason.status;
+		}
+		
+		$http.get("GetProducts").then(onProductsComplete, onError);
 	});
 }());
