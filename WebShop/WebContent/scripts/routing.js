dd3f409b58ad2cfbea35d86dec2e841d07ae7f0e
elev <elev@192.168.1.133>
2017-05-12 13:08:59 +0200

    Viev product

--- WebShop/WebContent/scripts/routing.js
+++ WebShop/WebContent/scripts/routing.js
@@ -10,47 +10,33 @@
 	routingApplication.config(function($routeProvider) {
 
 		$routeProvider.when("/", {
+		//Main viev
 			templateUrl : "main.htm",
-			controller : "homeController"
-		}).when("/products", {
+			controller : "homeController" })
+		//Viev all products
+		.when("/products", {
 			templateUrl : "products.htm",
-			controller : "productsController"
-		}).when("/contact", {
+			controller : "productsController" })
+		//Viev one product
+		.when("/product/:id", {
+			templateUrl : "product.htm",
+			controller : "productController" })
+		//Viev contact page
+		.when("/contact", {
 			templateUrl : "contact.htm",
-			controller : "contactController"
-		}).when("/about", {
+			controller : "contactController" })
+		//Viev about page
+		.when("/about", {
 			templateUrl : "about.htm",
-			controller : "aboutController"
-		}).when("/shoppingCart", {
+			controller : "aboutController" })
+		//Viev shoppingcart page
+		.when("/shoppingCart", {
 			templateUrl : "shoppingCart.htm",
-			controller : "shoppingCartController"
-		}).otherwise({
+			controller : "shoppingCartController" })
+		//Other
+		.otherwise({
 			redirectTo : "/"
 		})
 	});
-
-//	routingApplication.controller("homeController", function($scope) {
-//		$scope.title = "Home";
-//		$scope.description = "This is the Home page for MESH Webshop";
-//	});
-//
-//	routingApplication.controller("productsController", function($scope) {
-//		$scope.title = "Products";
-//		$scope.description = "This is the Products page";
-//	});
-//
-//	routingApplication.controller("contactController", function($scope) {
-//		$scope.title = "Contact";
-//		$scope.description = "This is the Contact page";
-//	});
-//
-//	routingApplication.controller("aboutController", function($scope) {
-//		$scope.title = "About";
-//		$scope.description = "This is the About page";
-//	});
-//	routingApplication.controller("shoppingCartController", function($scope) {
-//		$scope.title = "ShoppingCart";
-//		$scope.description = "This is the ShoppingCart page";
-//	});
 
 }());
