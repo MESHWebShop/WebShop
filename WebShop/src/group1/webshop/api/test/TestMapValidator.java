937c489ab6846f50fe19977a4fe38eec88e1613a
Ragnarsmoke <emil.bertilsson@outlook.com>
2017-05-12 17:44:24 +0200

    Added missing entries and correct values test case (Map validator)

--- WebShop/src/group1/webshop/api/test/TestMapValidator.java
+++ WebShop/src/group1/webshop/api/test/TestMapValidator.java
@@ -20,12 +20,41 @@ public class TestMapValidator {
     private final TestValidator validator = new TestValidator();
 
     /**
+     * Tests for missing entries
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testMissingEntries() {
+        Map<String, Object> errMap = validator.validate(
+                JsonInterface.fromJson("{\"map1\"={}}"));
+
+        assertEquals("Missing entry root depth test",
+                true,
+                errMap.containsKey("number1"));
+
+        assertEquals("Missing entry secondary depth test",
+                true,
+                ((Map<String, Object>) errMap.get("map1")).containsKey("string1"));
+    }
+
+    /**
+     * Tests for correct values for every key
+     */
+    @Test
+    public void testCorrectValues() {
+        Map<String, Object> errMap = validator.validate(
+                JsonInterface.fromJson(
+                        "{\"number1\"=25.0,\"number2\"=110.0,\"number3\"=3.0,\"map1\"={\"string1\"=\"55555\",\"string2\"=\"666666\",\"string3\"=\"22\",\"string4\"=\"88888888\"},\"map2\"={}}"));
+
+        assertEquals("Empty error map test", true, errMap.isEmpty());
+    }
+
+    /**
      * Tests for incorrect values for every key
      */
     @SuppressWarnings("unchecked")
     @Test
     public void testIncorrectValues() {
-        // Tests for incorrect values (length, range) for each element
         Map<String, Object> errMap = validator.validate(
                 JsonInterface.fromJson(
                         "{\"number1\"=15.0,\"number2\"=10.0,\"number3\"=10.0,\"map1\"={\"string1\"=\"4444\",\"string2\"=\"333\",\"string3\"=\"666666\",\"string4\"=\"1\"},\"map2\"={}}"));
